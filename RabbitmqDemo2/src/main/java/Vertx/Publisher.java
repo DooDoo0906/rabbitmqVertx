b9c076245f1b801f09a89cffdb42d490d98826f7
thuan.lai <thuan.lai@mservice.com.vn>
2021-12-27 14:45:43 +0700

    Updated

--- RabbitmqDemo2/src/main/java/Vertx/Publisher.java
+++ RabbitmqDemo2/src/main/java/Vertx/Publisher.java
@@ -10,7 +10,7 @@ import io.vertx.rabbitmq.RabbitMQOptions;
 public class Publisher extends AbstractVerticle {
     public void start() {
         RabbitMQOptions config = new RabbitMQOptions();
-    // Each parameter is optional
+    // Config specify individual parameters
         config.setUser("guest");
         config.setPassword("guest");
         config.setHost("localhost");
@@ -18,16 +18,12 @@ public class Publisher extends AbstractVerticle {
 
         RabbitMQClient client = RabbitMQClient.create(vertx, config);
 
-    // Connect
-        JsonObject js = new JsonObject().put("a", 10).put("b", 0).put("op", ":");
-
-
-
+        JsonObject msg = new JsonObject().put("a", 10).put("b", 5).put("op", ":");
+        // Connect
         client.start(asyncResult -> {
             if (asyncResult.succeeded()) {
                 System.out.println("RabbitMQ successfully connected!");
-                Buffer message = Buffer.buffer(js.toString());
-                client.basicPublish("", "aaaaaa", message, pubResult -> {
+                client.basicPublish("", "aaaaaa", msg.toBuffer(), pubResult -> {
                     if (pubResult.succeeded()) {
                         System.out.println("Message published !");
                     } else {
@@ -38,16 +34,5 @@ public class Publisher extends AbstractVerticle {
                 System.out.println("Fail to connect to RabbitMQ " + asyncResult.cause().getMessage());
             }
         });
-
-
-//        client.queueDeclare("aaaaaa", false, false, false, queueResult -> {
-//            if (queueResult.succeeded()) {
-//                System.out.println("Declared!");
-//            } else {
-//                System.err.println("Failed ");
-//                queueResult.cause().printStackTrace();
-//            }
-//        });
-
     }
 }
